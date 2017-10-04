d789c256f568b2dbb056d39529bbc6ef90a3b4b8
FruitWave <league@WTS8.attlocal.net>
2017-10-03 19:55:38 -0700

    We did RocketShip and SimpleNumberSorter.
    We also started Fruit Quiz.

--- src/SimpleNumberSorter.java
+++ src/SimpleNumberSorter.java
@@ -12,7 +12,25 @@ public static void main(String[] args) {
 	int number3=Integer.parseInt(num3);
 	
 	if(number1<number2 && number1<number3) {
-		
+		System.out.println(number1);
+		if(number2<number3){
+			System.out.println(number2);
+			System.out.println(number3);
+		}else{
+			System.out.println(number3);
+			System.out.println(number2);
+		}
 	}
-}
+	else if(number2<number3){
+			System.out.println(number2);
+	if(number1<number3){
+	System.out.println(number1);
+	System.out.println(number3);
+	}else{
+		System.out.println(number3);
+		System.out.println(number1);
+	}
+		}
+	}
+
 }
--- src/methods/RocketShip.java
+++ src/methods/RocketShip.java
@@ -3,18 +3,31 @@
 
 package methods;
 
+import javax.swing.JOptionPane;
+
 public class RocketShip {
 	public static void main(String[] args) {
 		
 		// 2. ask the user when to start (JOptionPane.showInputDialog)
+		String seconds= JOptionPane.showInputDialog("In how many seconds should I blast off?(number form)");
 		// 3. convert the user's answer to an int (Integer.parseInt)
-
+		int intSeconds= Integer.parseInt(seconds);
 		// 4. count down from user's starting point
+		
 		// 1. count down from 10 to 0 (print each number)
-
+		JOptionPane.showMessageDialog(null, "BLAST OFF!!! Launching in "+ intSeconds +" seconds.");
+		
+		for (int n = intSeconds; n > 0;n --) {
+			System.out.println(n);
+			String  speaking= Integer.toString(n);
+			speak(speaking);
+		}
+		speak("Blastoff!");
+		
 		// 5. when the counting is done, print "blastoff!"
 		// 6. replace the print commands with calls to the speak() method 
 	}
+	
 
 static void speak(String words) {
    	 try {
